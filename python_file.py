71c8cd2fc1186d973e34e2f887c2c8698f5f998a
Sirius083 <1518246621@qq.com>
2018-08-01 14:37:33 +0800

    Update python_file.py

--- python_file.py
+++ python_file.py
@@ -2,3 +2,13 @@
 import pandas as pd
 bbox_file = r'E:\tiny_imagenet\tiny-imagenet-200\train\n01443537\n01443537_boxes.txt'
 data = pd.read_csv(bbox_file, sep=" ")
+
+
+# save dict to file
+def save_obj(obj, name ):
+    with open('obj/'+ name + '.pkl', 'wb') as f:
+        pickle.dump(obj, f, pickle.HIGHEST_PROTOCOL)
+
+def load_obj(name ):
+    with open('obj/' + name + '.pkl', 'rb') as f:
+        return pickle.load(f)
