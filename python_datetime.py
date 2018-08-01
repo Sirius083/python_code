4d12567fbe3c53f26f40a11fd61917b5b3eb43a4
Sirius083 <1518246621@qq.com>
2018-08-01 08:33:11 +0800

    Update python_datetime.py

--- python_datetime.py
+++ python_datetime.py
@@ -1,3 +1,18 @@
+# get China Time
 from datetime import datetime
-arrow.get(datetime.now(), 'Asia/Shanghai') # get China Time
+arrow.get(datetime.now(), 'Asia/Shanghai')
+
+
+# change from utc time to Chinese time
+from dateutil import tz
+from datetime import datetime
+
+from_zone = tz.gettz('UTC') # UTC Zone --> UTC
+to_zone = tz.gettz('Asia/Shanghai')# China Zone --> PRC
+
+utc = datetime.utcnow()
+utc = utc.replace(tzinfo=from_zone)
+local = utc.astimezone(to_zone)
+print(datetime.strftime(local, "%Y-%m-%d %H:%M:%S"))
+
 
