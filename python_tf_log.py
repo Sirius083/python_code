c34f782d834482bd1565e2a7729e10c873f8f748
Sirius083 <1518246621@qq.com>
2018-08-01 09:05:35 +0800

    Update python_tf_log.py

--- python_tf_log.py
+++ python_tf_log.py
@@ -1,4 +1,17 @@
 from datetime import datetime
 # from each run create a directory
-now = datetime.now().strftime("%m%d%H%M")
-logdir = "run-{}/".format(now)
+def get_logdir():
+    now = datetime.now().strftime("%m%d%H%M") # local time as log dir name
+    logdir = "run-{}/".format(now)
+    return logdir
+
+
+# ckpt file path
+if True:
+   checkpoint = tf.train.latest_checkpoint(ckpt_path) # restore from the lastest checkpoint file
+else: # build new ckpt_path
+   checkpoint = ckpt_path + '/' +  checkpoint_name   # restore from given checkpoint name
+    
+    
+
+
