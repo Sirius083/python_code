6dbdae79b4dd5f11671d6b148845f44f87fb261e
Sirius083 <1518246621@qq.com>
2018-08-07 10:04:24 +0800

    Update python_plot.py

--- python_plot.py
+++ python_plot.py
@@ -4,3 +4,13 @@ import matplotlib.image as mpimg
 img=mpimg.imread(filenames_labels[0][0])
 imgplot = plt.imshow(img)
 plt.show()
+
+
+# Note: path format in windows, file_path = r''
+import matplotlib.pyplot as plt
+# image_path = 'E:/tiny_imagenet/tiny-imagenet-200/train\n01443537\images\n01443537_0.JPEG' # wrong
+image_path = 'E:/tiny_imagenet/tiny-imagenet-200/train\n01443537\images\n01443537_0.JPEG'   # correct
+img = plt.imread(image_path)
+imgplot = plt.imshow(img)
+plt.show()
+
