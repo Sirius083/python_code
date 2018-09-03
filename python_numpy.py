ecb211b2e422f3cf64d9ba30ed3fa1290593a092
Sirius083 <1518246621@qq.com>
2018-09-03 16:33:05 +0800

    Update python_numpy.py

--- python_numpy.py
+++ python_numpy.py
@@ -1,2 +1,12 @@
 # calculate mean over all other axises
 tmp = np.mean(batch,axis =(0,1,2))
+
+
+# using pickle save numpy array to disk
+import pickle
+with open('tiny_test.pickle', 'wb') as handle:
+    # pickle.dump(images_decode, handle, protocol=pickle.HIGHEST_PROTOCOL)
+    pickle.dump(test_decode, handle)
+
+with open('tiny_train.pickle', 'rb') as handle:
+    images_decode = pickle.load(handle)
